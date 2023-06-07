850c0e1f68d8ee7cef5e3b65617f964d3f332e45
SUJAN <123sujanpr@gmail.com>
2023-06-07 13:18:17 +0545

    test

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project</title>
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <section class="form signup">
+            <header>Here You can chart</header>
+            <form action="">
+                <div class="error-txt">This is an error meaage</div>
+                <div class="name-details">
+                    <div class="field">
+                        <label for="#">First Name</label>
+                        <input type="text" placeholder="Enter First Name">
+                    </div>
+                    <div class="field">
+                        <label for="#">Last Name</label>
+                        <input type="text" placeholder="Enter Last Name">
+                    </div>
+                </div>
+                <div class="field">
+                    <label for="#">Email Address</label>
+                    <input type="email" placeholder="Enter Email Address">
+                </div>
+                <div class="field">
+                    <label for="#">Password</label>
+                    <input type="password" placeholder="Enter Password">
+                </div>
+                <div class="field">
+                    <label for="#">Select Images</label>
+                    <input type="file">
+                </div>
+                <div class="field">
+
+                    <input type="submit" value="Continue to Chat">
+                </div>
+                <div class="link">Already signed up? <a href="#">Login Now</a> </div>
+
+            </form>
+        </section>
+    </div>
+
+</body>
+
+</html>
